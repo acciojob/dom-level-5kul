a20aaee193c3769c2482049c82d112966a60f638
KULDEEP MISHRA <158719940+5kul@users.noreply.github.com>
2025-07-26 23:06:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+window.onload = function() {
+    // Get the element with id 'level'
+    var element = document.getElementById("level");
+
+    // Initialize level counter
+    var level = 0;
+
+    // Traverse up the DOM tree to count parent elements
+    while (element) {
+        level++;
+        element = element.parentElement;
+    }
+
+    // Display the result
+    alert("The level of the element is: " + level);
+};
